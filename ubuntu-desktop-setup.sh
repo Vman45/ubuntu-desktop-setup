4e0daba06b9ef7dd82cd95700148a0bf9b4e02bf
Kok Kek <kokkekpek@gmail.com>
2021-04-06 14:18:53 +0300

    Move docker repository from etc/apt/sources.list to /etc/apt/sources.list.d/docker.list

--- ubuntu-desktop-setup.sh
+++ ubuntu-desktop-setup.sh
@@ -64,7 +64,7 @@ sudo apt install git -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 wget -qO - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88 -y
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update -y
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 sudo usermod -aG docker $USER
