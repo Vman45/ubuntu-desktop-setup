d312e6fa1cf25e23fadeabdc7071cda672ee1d02
Kok Kek <kokkekpek@gmail.com>
2021-07-02 14:03:57 +0300

    Update IDEA to 2021.1.3

--- ubuntu-desktop-setup.sh
+++ ubuntu-desktop-setup.sh
@@ -145,9 +145,9 @@ sudo apt install oracle-java16-installer -y
 
 # IDEA
 sudo apt install rename -y
-wget https://download-cf.jetbrains.com/idea/ideaIU-2021.1.2.tar.gz
-tar -xzvf ideaIU-2021.1.2.tar.gz
-rm ideaIU-2021.1.2.tar.gz
+wget https://download-cf.jetbrains.com/idea/ideaIU-2021.1.3.tar.gz
+tar -xzvf ideaIU-2021.1.3.tar.gz
+rm ideaIU-2021.1.3.tar.gz
 rename "s/idea-IU-211.+/IDEA/" *
 mkdir ~/Applications -p
 rm -rf ~/Applications/IDEA
